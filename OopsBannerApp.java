cd16a5d42f3d7db02853e3d0b0bdb74aabc8b8db
ng0816-SRM <ng0816@srmist.edu.in>
2026-02-23 13:55:56 +0530

    uc2-print-statement

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,5 +1,12 @@
 public class OopsBannerApp{
-    public static void main(String[] args){
-        System.out.println("OOPS");
+    public static void main(String[] args) {
+
+        System.out.println("  *****     *****    ********   ******* ");
+        System.out.println(" *     *   *     *   *       *  *       ");
+        System.out.println("*       * *       *  *       *  *       ");
+        System.out.println("*       * *       *  ********   ******* ");
+        System.out.println("*       * *       *  *                * ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println("  *****     *****    *          ******* ");
     }
 }
